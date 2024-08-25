a024d27db37e7d41d47e3b9fc21739dc76b6ee40
Jose Pablo Tur <josepablotur@gmail.comr>
2024-08-25 13:19:31 +0200

    Variables modificadas

--- code.py
+++ code.py
@@ -4,15 +4,14 @@ def find_max(numbers: list) -> int:
         raise ValueError("La lista no puede estar vacía")
     
     # Inicializa el valor máximo con el primer elemento de la lista
-    max_number = numbers[0]
+    maxmaximo = numbers[0]
     
     # Recorre la lista desde el segundo elemento hasta el final
     for num in numbers[1:]:
-        # Si el número actual es mayor que el valor máximo actual, lo actualiza
-        if num > max_number:
-            max_number = num
+        if num > maximo:
+            maximo = num
     
-    return max_number
+    return maximo
 
 
 
