e0278613167596ac9ceb9cf64bb332d5c6620d42
Jose Pablo Tur <josepablotur@gmail.comr>
2024-08-26 16:59:09 +0200

    main creado, fichero external_funcions creada aparte para mejor claridad en el código

--- external_functions.py
+++ external_functions.py
@@ -4,7 +4,7 @@ def find_max(numbers: list) -> int:
         raise ValueError("La lista no puede estar vacía")
     
     # Inicializa el valor máximo con el primer elemento de la lista
-    maxmaximo = numbers[0]
+    maximo = numbers[0]
     
     # Recorre la lista desde el segundo elemento hasta el final
     for num in numbers[1:]:
@@ -16,12 +16,3 @@ def find_max(numbers: list) -> int:
 def testing():
     testing_var = 5
     return testing_var
-
-
-numbers = [3, 5, 7, 2, 8, 1, 4]
-max_value = find_max(numbers)
-print(f"El número máximo en la lista es: {max_value}")
-
-
-
-
---
+++ main.py
@@ -0,0 +1,6 @@
+import external_functions
+
+
+numbers = [3, 5, 7, 2, 8, 1, 4]
+max_value = external_functions.find_max(numbers)
+print(f"El número máximo en la lista es: {max_value}")
