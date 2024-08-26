42cbd825a9932e16b96491bbf7c6947db4943865
Jose Pablo Tur <josepablotur@gmail.comr>
2024-08-26 17:18:05 +0200

    "Alert messages added to main code"

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import external_functions
 
 
 numbers = [3, 5, 7, 2, 8, 1, 4]
-max_value = external_functions.find_max(numbers)
+max_value = external_functions.gifind_max(numbers)
 print(f"El número máximo en la lista es: {max_value}")
+
+
+echo 'Success! This line will be shown in clase of passing the test.'
+echo 'Error! This message will be shown any step of the process it does not pass the test.'
